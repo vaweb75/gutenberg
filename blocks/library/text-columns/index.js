7ea01586ee0879a96cc9fe6322266febc1460729
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-10-19 12:27:25 +0200

    Editable: Add missing Editable.Value wrappers to the existing blocks

--- blocks/library/text-columns/index.js
+++ blocks/library/text-columns/index.js
@@ -110,7 +110,7 @@ registerBlockType( 'core/text-columns', {
 			<section className={ `align${ width } columns-${ columns }` }>
 				{ times( columns, ( index ) =>
 					<div className="wp-block-column" key={ `column-${ index }` }>
-						<p>{ content && <Editable.Value value={ content[ index ] } /> }</p>
+						<p>{ content && content[ index ] && <Editable.Value value={ content[ index ] } /> }</p>
 					</div>
 				) }
 			</section>
