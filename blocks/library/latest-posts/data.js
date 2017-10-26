dfe86f80b36dc4e19dbdc4293a8747a2296b7dc9
Jorge <jorge.costa@developer.pt>
2017-10-26 23:28:09 +0100

    Implemented query panel in latest posts block.

--- blocks/library/latest-posts/data.js
+++ blocks/library/latest-posts/data.js
@@ -2,15 +2,22 @@
  * Returns a Promise with the latest posts or an error on failure.
  *
  * @param   {Number} postsToShow       Number of posts to display.
+ * @param   {String} order             Sort attribute ascending or descending.
+ * @param   {String} orderBy           Attribute specifying how to sort the posts.
+ * @param   {String} categories        The terms assigned to the object in the category taxonomy.
+ *
  *
  * @returns {wp.api.collections.Posts} Returns a Promise with the latest posts.
  */
-export function getLatestPosts( postsToShow = 5 ) {
+export function getLatestPosts( postsToShow, order, orderBy, categories ) {
 	const postsCollection = new wp.api.collections.Posts();
 
 	const posts = postsCollection.fetch( {
 		data: {
 			per_page: postsToShow,
+			order,
+			orderby: orderBy,
+			categories,
 		},
 	} );
 
