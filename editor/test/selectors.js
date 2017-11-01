7dd8f34fad1048acd233b546cd47e80092fb4718
Jorge <jorge.costa@developer.pt>
2017-11-01 14:24:41 +0000

    Lint fix.

--- editor/test/selectors.js
+++ editor/test/selectors.js
@@ -24,7 +24,7 @@ import {
 	isCleanNewPost,
 	getCurrentPost,
 	getCurrentPostAuthor,
-	getCurrentPostId,	
+	getCurrentPostId,
 	getCurrentPostLastRevisionId,
 	getCurrentPostRevisionsCount,
 	getCurrentPostType,
